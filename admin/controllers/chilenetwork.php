934cab0bf884b21ba5a7717cd971f3afd610f4b4
Mario Soto <mariosoto77@gmail.com>
2016-07-04 00:42:59 -0500

    Create chilenetwork.php

---
+++ admin/controllers/chilenetwork.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_chilenetwork
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die;
+
+/**
+ * ChileNetwork Controller
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_chilenetwork
+ * @since       0.0.9
+ */
+class ChileNetworkControllerChileNetwork extends JControllerForm
+{
+}
